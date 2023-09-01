817ce47da34fc93c0d674e07ddb39872f9349c2f
Nisitha-Munasinghe <ma.nisithapramoad@gmail.com>
2023-09-01 18:03:16 +0530

    Implement dashboard fxml form

--- src/main/java/lk/ijse/D24_Hostel_Management_System/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/D24_Hostel_Management_System/controller/DashboardFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.D24_Hostel_Management_System.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class DashboardFormController {
+
+    @FXML
+    private ImageView imgPayment;
+
+    @FXML
+    private ImageView imgRoom;
+
+    @FXML
+    private ImageView imgSettting;
+
+    @FXML
+    private ImageView imgStudent;
+
+    @FXML
+    private ImageView imgSummery;
+
 }
