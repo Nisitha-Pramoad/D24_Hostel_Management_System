a8bdff0bb84366ce9bd4214a5f2226e725071b9f
Nisitha-Munasinghe <ma.nisithapramoad@gmail.com>
2023-09-02 15:57:08 +0530

    dashboard implementation done

--- src/main/java/lk/ijse/D24_Hostel_Management_System/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/D24_Hostel_Management_System/controller/DashboardFormController.java
@@ -1,7 +1,23 @@
 package lk.ijse.D24_Hostel_Management_System.controller;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class DashboardFormController {
 
@@ -19,5 +35,88 @@ public class DashboardFormController {
 
     @FXML
     private ImageView imgSummery;
+
+    @FXML
+    private AnchorPane root;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(2000), root);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+    }
+
+    @FXML
+    void navigate(MouseEvent event) throws IOException {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            Parent root = null;
+
+            switch (icon.getId()) {
+                case "imgStudent":
+                    root = FXMLLoader.load(this.getClass().getResource("/view/student-registration-form.fxml"));
+                    break;
+                case "imgRoom":
+                    root = FXMLLoader.load(this.getClass().getResource("/view/room-form.fxml"));
+                    break;
+                case "imgPayment":
+                    root = FXMLLoader.load(this.getClass().getResource("/view/payment-form.fxml"));
+                    break;
+                case "imgSummery":
+                    root = FXMLLoader.load(this.getClass().getResource("/view/summery-form.fxml"));
+                    break;
+                case "imgSettting":
+                    root = FXMLLoader.load(this.getClass().getResource("/view/setting-form.fxml"));
+                    break;
+            }
+
+            if (root != null) {
+                Scene subScene = new Scene(root);
+                Stage primaryStage = (Stage) this.root.getScene().getWindow();
+                primaryStage.setScene(subScene);
+                primaryStage.centerOnScreen();
+
+                TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
+                tt.setFromX(-subScene.getWidth());
+                tt.setToX(0);
+                tt.play();
+
+            }
+        }
+    }
+    
+
+    @FXML
+    void playMouseEnterAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(20);
+            glow.setHeight(20);
+            glow.setRadius(20);
+            icon.setEffect(glow);
+        }
+    }
+
+    @FXML
+    void playMouseExitAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+
+            icon.setEffect(null);
+        }
+    }
 
 }
